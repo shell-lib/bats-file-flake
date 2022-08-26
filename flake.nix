84176060bb8f3982cc0aba21814b27e6e3e8aa6b
Adam Gaia <git@agaia.dev>
2022-08-26 16:02:26 -0600

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Bats-file helper library";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        bats-file = pkgs.stdenv.mkDerivation {
+          name = "bats-file";
+          src = pkgs.fetchgit {
+            url = "https://github.com/bats-core/bats-file.git";
+            rev = "v0.3.0";
+            sha256 = "sha256-3xevy0QpwNZrEe+2IJq58tKyxQzYx8cz6dD2nz7fYUM=";
+          };
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./load.bash $out/bin/
+            cp -r ./src $out/bin/
+          '';
+        };
+        packages = { default = bats-file; };
+      });
+}
